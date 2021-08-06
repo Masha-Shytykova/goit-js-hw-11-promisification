4fc165dfb8272aaf091078b70ce06ae03b001267
Masha-Shytykova <mashtakova.mash@gmail.com>
2021-08-06 16:08:04 +0300

    added solution

---
+++ app1.js
@@ -0,0 +1,21 @@
+const delay = ms => {
+  //const DELAY = ms;
+  //   return new Promise(resolve => {
+  //     setTimeout(() => {
+  //       resolve(ms);
+  //     }, DELAY);
+  //   });
+
+  // return new Promise(resolve => {
+  //   resolve(ms);
+  // });
+
+  return Promise.resolve(ms);
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
